d35d695d62cdb68f236aa4b84c6166a9fd3f9f22
medawilgens24-coder <medawilgens24@gmail.com>
2026-09-20 11:26:59 -0400

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,3 +1,41 @@
+package com.meda.app;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintManager;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import com.getcapacitor.BridgeActivity;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView wv = getBridge().getWebView();
+        wv.addJavascriptInterface(new NativeBridge(), "AndroidBridge");
+    }
+
+    private void toast(final String msg) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+            }
         });
     }
 
@@ -49,3 +87,7 @@
                 toast("Fichye sove: " + safe + " (" + where + ")");
             } catch (Exception e) {
                 toast("Erè pandan sovgad: " + e.getMessage());
+            }
+        }
+    }
+}
